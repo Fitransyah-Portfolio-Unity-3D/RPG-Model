769870be9116997116c9e7a062dadc4a49cf3af6
Fitransyah <flair4evah@gmail.com>
2023-09-02 07:14:47 +0700

    git init

---
+++ Assets/_Forest/Script/Movement/Mover.cs
@@ -0,0 +1,128 @@
+using RPG.Core;
+using UnityEngine;
+using UnityEngine.AI;
+using RPG.Saving;
+using RPG.Attributes;
+using System;
+
+namespace RPG.Movement
+{
+
+    public class Mover : MonoBehaviour, IAction, ISaveable
+    {
+        [SerializeField] Transform target;
+        [SerializeField] float maxSpeed = 6f;
+        [SerializeField] float maxNavPathLength = 40f;
+
+        NavMeshAgent navMeshAgent;
+        Health health;
+
+        private void Awake()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            health = GetComponent<Health>();
+        }
+
+        void Update()
+        {
+            navMeshAgent.enabled = !health.IsDead();
+
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination, float speedFraction)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            MoveTo(destination, speedFraction);
+        }
+
+        public bool CanMoveTo(Vector3 destination)
+        {
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, destination, NavMesh.AllAreas, path);
+
+            if (!hasPath) return false;
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+
+            if (GetPathLength(path) > maxNavPathLength) return false;
+
+            return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2) return total;
+
+            for (int i = 0; i < path.corners.Length -1; i++)
+            {
+                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+
+            return total;
+        }
+
+        public void MoveTo(Vector3 destination, float speedFraction)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.speed = maxSpeed * Mathf.Clamp01(speedFraction);
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        // specific save purposes
+        [System.Serializable]
+        struct MoverSaveData
+        {
+            public SerializableVector3 position;
+            public SerializableVector3 rotation;
+        }
+
+        public object CaptureState()
+        {
+ 
+            // struct version
+            MoverSaveData data = new MoverSaveData();
+            data.position = new SerializableVector3(transform.position);
+            data.rotation = new SerializableVector3(transform.eulerAngles);
+
+            // Dictionary version
+            //Dictionary<string, object> data = new Dictionary<string, object>();
+            //data["position"] = new SerializableVector3(transform.position);
+            //data["rotation"] = new SerializableVector3(transform.eulerAngles);
+            return data;
+        }
+
+        public void RestoreState(object state)
+        {
+            MoverSaveData data = (MoverSaveData) state;
+            //Dictionary<string, object> data = (Dictionary<string, object>)state;
+
+            // Weird behaviour from Navmesh
+            GetComponent<NavMeshAgent>().enabled = false;
+
+            // Struct version
+            transform.position = data.position.ToVector();
+            transform.eulerAngles = data.rotation.ToVector();
+
+            // Dictionary version
+            //transform.position = ((SerializableVector3)data["position"]).ToVector();
+            //transform.eulerAngles = ((SerializableVector3)data["rotation"]).ToVector();
+
+            GetComponent<NavMeshAgent>().enabled = true;
+        }
+    }
+}
+
